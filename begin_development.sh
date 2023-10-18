f9c75d16d496d2d098a311b1d0b56ca0be398cc2
Marek Lipert <marek@spinney.io>
2023-10-18 12:01:59 +0200

    Remove arm64 requirement.

--- begin_development.sh
+++ begin_development.sh
@@ -1,4 +1,4 @@
-arch -arm64 brew install llvm@16
+brew install llvm@16
 brew unlink llvm@16 && brew link --force llvm@16
-arch -arm64 brew install protobuf@21
+brew install protobuf@21
 brew unlink protobuf@21 && brew link --force protobuf@21

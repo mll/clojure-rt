d2dd8fd30dd117160febb9ec7fba98779febe854
a-wiacek <33523701+a-wiacek@users.noreply.github.com>
2023-12-05 16:07:43 +0100

    Fresh/bound variables - initial version

--- frontend/src/clojure/rt/core.clj
+++ frontend/src/clojure/rt/core.clj
@@ -9,7 +9,7 @@
             [clojure.string :refer [join split]]
             [clojure.tools.reader.reader-types :as t]
             [clojure.rt.passes :as passes]
-            
+
             [clojure.tools.analyzer
              [utils :refer [ctx resolve-sym -source-info resolve-ns obj? dissoc-env butlast+last mmerge]]
              [ast :refer [walk prewalk postwalk] :as ast]
@@ -61,21 +61,24 @@
 
     #'classify-invoke
     #'collect-closed-overs
+
+    ;; #'passes/fresh-and-let-bound-vars
     #'passes/mm-pass-one})
 
 (def scheduled-rt-passes
   (schedule rt-passes))
 
 
-(defn analyze [s filename] 
+(defn analyze [s filename]
   (with-bindings {#'a/run-passes scheduled-rt-passes}
     (let [reader (t/source-logging-push-back-reader s 1 filename)]
       (loop [form (r/read {:eof :eof} reader) ret-val []]
-        (if (= :eof form) ret-val
+        (if (= :eof form) (do #_(clojure.pprint/pprint (passes/clean-tree ret-val)) ;; uncomment to see simple tree
+                              ret-val)
             (do
               (eval form)
               (recur (r/read {:eof :eof} reader)
-                     (conj ret-val (a/analyze form)))))))))
+                     (conj ret-val (passes/fresh-and-let-bound-vars (a/analyze form))))))))))
 
 
 (defn generate-protobuf-defs [] (sch/generate-protobuf-defs "bytecode.proto"))
@@ -86,8 +89,8 @@
    (protojure/->pb (enc/encode-ast (analyze form infile-name)))
    (java.io.File. outfile)))
 
-(defn -main 
-  ([infile] (let [parts (split infile #"\.")] 
+(defn -main
+  ([infile] (let [parts (split infile #"\.")]
               (compile (slurp infile) (str (join "." (butlast parts)) ".cljb") infile)))
   ([] (println "Generating protobuf definitions into bytecode.proto file. To compile use file name as parameter")
    (generate-protobuf-defs)))
--- frontend/src/clojure/rt/passes.clj
+++ frontend/src/clojure/rt/passes.clj
@@ -1,32 +1,102 @@
 (ns clojure.rt.passes
   (:refer-clojure :exclude [compile])
-  (:require [clojure.tools.reader :as r]
-            [clojure.tools.analyzer.jvm :as a]
-            [clojure.tools.emitter.jvm :as e]
-            
-            [clojure.tools.analyzer
-             [utils :refer [ctx resolve-sym -source-info resolve-ns obj? dissoc-env butlast+last mmerge]]
-             [ast :refer [walk prewalk postwalk] :as ast]
-             [env :as env :refer [*env*]]
-             [passes :refer [schedule]]]
-
-            [clojure.tools.analyzer.jvm.utils :refer :all :as u :exclude [box specials]]
+  (:require [clojure.set :as set]
+            [clojure.tools.analyzer.ast :refer [update-children]]
             [clojure.tools.analyzer.passes
              [uniquify :refer [uniquify-locals]]
              [collect-closed-overs :refer [collect-closed-overs]]]))
 
-(defn mm-pass-one  
+(defn mm-pass-one
   ^{:pass-info {:walk :none :depends #{#'uniquify-locals #'collect-closed-overs} :state (fn [] (atom {}))}}
   ([ast] (mm-pass-one (atom {}) ast))
   ([state ast]
    (case (:op ast)
-     :local (swap! state (fn [old-state] 
+     :binding (swap! state (fn [old-state]
                            (let [name (:name ast)]
                              (if (get old-state name)
                                (update old-state name inc)
                                (assoc old-state name 1)))))
      nil)
-;   (println "AAAAAAAA" (:op ast) " - " state)
+  ;;  (println "AAAAAAAA" (:op ast) " - " state)
    (assoc ast :mm-refs @state)))
-   
-   
+
+(defmulti -fresh-and-let-bound-vars (fn [node _ _] (:op node)))
+
+(defn binding-list-fresh-and-let-bound-vars
+  [bindings-list fresh let-bound]
+  (reduce (fn [[new-bindings bindings-fresh let-bound] binding]
+            (let [new-binding (-fresh-and-let-bound-vars binding bindings-fresh let-bound)]
+              [(conj new-bindings new-binding)
+               (set/difference (set/union bindings-fresh (:fresh new-binding)) (:new-bound-vars new-binding))
+               (set/union let-bound (:new-bound-vars new-binding))]))
+          [[] fresh let-bound]
+          bindings-list))
+
+(defmethod -fresh-and-let-bound-vars :fn-method
+  [node fresh let-bound]
+  (let [[new-params bindings-fresh bindings-let-bound]
+        (binding-list-fresh-and-let-bound-vars (:params node) fresh let-bound)]
+    (-> node
+        (update :body -fresh-and-let-bound-vars bindings-fresh bindings-let-bound)
+        (assoc :params new-params
+               :fresh bindings-fresh
+               :let-bound bindings-let-bound))))
+
+(defmethod -fresh-and-let-bound-vars :binding
+  [node fresh let-bound]
+  (let [new-bound-vars (set [(:name node)]) ;; TODO: destructuring, this is only the simplest case
+        ;; init exists when variable is introduced by let,
+        ;; but not when it is a parameter of a function
+        new-init (some-> node :init (-fresh-and-let-bound-vars fresh let-bound)) ;; TODO: analyze init
+        value-fresh (:fresh new-init)]
+    (cond-> node
+      new-init (assoc :init new-init)
+      true     (assoc :fresh (set/union fresh value-fresh)
+                      :let-bound let-bound
+                      :new-bound-vars new-bound-vars))))
+
+(defmethod -fresh-and-let-bound-vars :let
+  [node fresh let-bound]
+  (let [[new-bindings bindings-fresh bindings-let-bound]
+        (binding-list-fresh-and-let-bound-vars (:bindings node) fresh let-bound)]
+    (-> node
+        (update :body -fresh-and-let-bound-vars bindings-fresh (set/union let-bound bindings-let-bound))
+        (assoc :bindings new-bindings
+               :fresh bindings-fresh
+               :let-bound let-bound))))
+
+(defmethod -fresh-and-let-bound-vars :default
+  [node fresh let-bound]
+  (-> node
+      (assoc :fresh fresh :let-bound let-bound)
+      (update-children #(-fresh-and-let-bound-vars % fresh let-bound))))
+
+(defn fresh-and-let-bound-vars
+  ^{:pass-info {:walk :none :depends #{#'uniquify-locals}}}
+  [ast]
+  (-fresh-and-let-bound-vars ast #{} #{}))
+
+(defmulti -memory-management-pass (fn [node _ _] (:op node)))
+
+(defmethod -memory-management-pass :binding
+  [_ _ _])
+
+(defmethod -memory-management-pass :default [node _ _] node)
+
+(defn memory-management-pass
+  ^{:pass-info {:walk :none :depends #{#'uniquify-locals #'fresh-and-let-bound-vars}}}
+  [ast]
+  (-memory-management-pass ast #{} #{}))
+
+(defn clean-tree
+  [node]
+  (cond (map? node)
+        (let [important-keys (concat (:children node)
+                                     [:op :fresh :let-bound :form])]
+          (->> (select-keys node important-keys)
+               (map (fn [[k v]] [k (clean-tree v)]))
+               (into {})))
+        (vector? node)
+        (mapv clean-tree node)
+        :else
+        node))

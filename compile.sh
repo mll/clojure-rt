5ed1d94497997bc2608756b00a1fb0af1f27f61f
Marek Lipert <marek@spinney.io>
2024-01-05 19:42:33 +0100

    Improved compile script, cleanup.

--- compile.sh
+++ compile.sh
@@ -3,12 +3,22 @@
 LEIN=`which lein`
 FILENAME=$1
 BINARY="${FILENAME%.clj}.cljb"
+cd backend
+if [ -f "$BINARY" ]; then
+    rm $BINARY
+fi
+cd ..
 cp $FILENAME frontend
 cd frontend
 echo "Compiling to AST..." 
 time $LEIN run $FILENAME
+rm "$FILENAME"
+if [ -f "$BINARY" ]; then
+    echo "Success!!"
+else
+    exit 1
+fi
 mv $BINARY ../backend
-rm $FILENAME
 cd ../backend
 #arch -arm64 lldb ./clojure-rt 
 echo "Executing..."
--- fib.clj
+++
@@ -1,3 +0,0 @@
-(defn fib [x] (if (= x 1) 1 (if (= x 2) 1 (+ (fib (- x 1)) (fib (- x 2))))))
-(fib 42)
-

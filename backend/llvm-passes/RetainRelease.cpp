487aeff4c66c0b96d5bee40155688ca549ea1efe
a-wiacek <33523701+a-wiacek@users.noreply.github.com>
2024-02-27 10:51:27 +0100

    Fix memory mgmt in RetainRelease pass

--- backend/llvm-passes/RetainRelease.cpp
+++ backend/llvm-passes/RetainRelease.cpp
@@ -1,4 +1,5 @@
 #include "llvm/Pass.h"
+#include "llvm/ADT/simple_ilist.h"
 #include "llvm/IR/Argument.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
@@ -13,6 +14,18 @@ char RetainReleasePass::ID = 0;
 FunctionPass *createRetainReleasePass() {
   return new RetainReleasePass();
 };
+
+// void disf(Function* x) {
+//   errs() << "---> Function body:\n" << *x << "\n";
+// }
+
+// void disb(BasicBlock* x) {
+//   errs() << "---> Basic block:\n" << *x << "\n";
+// }
+
+// void disi(Instruction* x) {
+//   errs() << "---> Instruction:\n" << *x << "\n";
+// }
 
 bool RetainReleasePass::runOnFunction(Function &F) {
   bool isChanged = false;
@@ -60,55 +73,54 @@ bool RetainReleasePass::runOnFunction(Function &F) {
             auto instructions = retains.find(val);
             if (instructions != retains.end()) {
               auto retains = &instructions->second;
-                if (!retains->empty()) {
-                  Instruction* lastRelease = retains->back();
-                  retains->pop_back();
-                  lastRelease->eraseFromParent();
-                  isChanged = true;
-                  if (call) {
-                    instrIt = instrIt->eraseFromParent(); // call release
-                  } else if (dec) {
-                    // current_block:
-                    //   ...
-                    //   %0 = atomicrmw volatile sub ... <- instrIt
-                    //   %cmp_jj = icmp eq i64 %0, 1
-                    //   br i1 %cmp_jj, label %destroy, label %ignore
-                    //
-                    // destroy:
-                    //   tail call void @Object_destroy ...
-                    //   label %release_cont
-                    //
-                    // ignore:
-                    //   label %release_cont
-                    //
-                    // release_cont:
-                    //   %release_phi = phi i1 [ true, %destroy ], [ false, %ignore ] <- dummy value, not used
-                    //   No other phi instructions!
-                    // Delete remaining instructions in current block, starting from instrIt
-                    // Delete destroy and ignore blocks
-                    // Merge current_block (with deleted instructions) and release_cont (without release_phi)
-                    
-                    instrIt = instrIt->eraseFromParent(); // sub
-                    assert(instrIt != blockIt->end() && "Expected icmp instruction, got end of block instead!");
-                    instrIt = instrIt->eraseFromParent(); // icmp
-                    assert(instrIt != blockIt->end() && "Expected br instruction, got end of block instead!");
-                    BranchInst* br = dyn_cast<BranchInst>(instrIt);
-                    assert(br && br->getNumSuccessors() == 2);
-                    BasicBlock *destroy = br->getSuccessor(0),
-                               *ignore = br->getSuccessor(1),
-                               *cont = dyn_cast<BranchInst>(ignore->begin())->getSuccessor(0);
-                    destroy->eraseFromParent();
-                    ignore->eraseFromParent();
-                    instrIt = instrIt->eraseFromParent(); // br
-                    assert(instrIt == blockIt->end() && "Br wasn't the last instruction in block!");
-                    auto contIt = cont->getFirstNonPHIOrDbgOrAlloca();
-                    blockIt->splice(blockIt->end(), cont);
-                    cont->eraseFromParent();
-                    instrIt = contIt;
-                  }
-                } else {
-                  ++instrIt;
+              if (!retains->empty()) {
+                Instruction* lastRelease = retains->back();
+                retains->pop_back();
+                lastRelease->eraseFromParent();
+                isChanged = true;
+                if (call) {
+                  instrIt = instrIt->eraseFromParent(); // call release
+                } else if (dec) {
+                  // current_block:
+                  //   ...
+                  //   %0 = atomicrmw volatile sub ... <- instrIt
+                  //   %cmp_jj = icmp eq i64 %0, 1
+                  //   br i1 %cmp_jj, label %destroy, label %ignore
+                  //
+                  // destroy:
+                  //   tail call void @Object_destroy ...
+                  //   label %release_cont
+                  //
+                  // ignore:
+                  //   label %release_cont
+                  //
+                  // release_cont:
+                  //   %release_phi = phi i1 [ true, %destroy ], [ false, %ignore ] <- dummy value, not used
+                  //   No other phi instructions!
+                  // Delete remaining instructions in current block, starting from instrIt
+                  // Delete destroy and ignore blocks
+                  // Merge current_block (with deleted instructions) and release_cont (without release_phi)
+                  
+                  instrIt = instrIt->eraseFromParent(); // sub
+                  assert(instrIt != blockIt->end() && "Expected icmp instruction, got end of block instead!");
+                  instrIt = instrIt->eraseFromParent(); // icmp
+                  assert(instrIt != blockIt->end() && "Expected br instruction, got end of block instead!");
+                  BranchInst* br = dyn_cast<BranchInst>(instrIt);
+                  assert(br && br->getNumSuccessors() == 2);
+                  BasicBlock *destroy = br->getSuccessor(0),
+                              *ignore = br->getSuccessor(1),
+                              *cont = dyn_cast<BranchInst>(ignore->begin())->getSuccessor(0);
+                  auto contIt = cont->getFirstNonPHIOrDbgOrAlloca();
+                  blockIt->splice(blockIt->end(), cont, contIt, cont->end());
+                  instrIt->eraseFromParent();
+                  destroy->eraseFromParent();
+                  ignore->eraseFromParent();
+                  cont->eraseFromParent();
+                  instrIt = contIt;
                 }
+              } else {
+                ++instrIt;
+              }
             } else {
               ++instrIt;
             }

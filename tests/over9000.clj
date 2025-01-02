a736990bbf499413eee196b63a6e1c4b67a4800c
a-wiacek <33523701+a-wiacek@users.noreply.github.com>
2025-01-02 16:25:47 +0100

    Fix RecurNode getType in fn

--- tests/over9000.clj
+++ tests/over9000.clj
@@ -17,3 +17,23 @@
 ;; CHECK: define i64 @fn_2_J_J
 ;; CHECK-NOT: load atomic ptr
 ;; CHECK: }
+
+(defn over9000-recur-1
+  ([x]
+   (if (< 9000 x)
+     x
+     (recur (+ x x))))
+  ([x y] 123))
+
+(over9000-recur-1 1)
+(over9000-recur-1 1 2)
+
+(defn over9000-recur-2
+  ([x y] 123)
+  ([x]
+   (if (< 9000 x)
+     x
+     (recur (+ x x)))))
+
+(over9000-recur-2 1)
+(over9000-recur-2 1 2)

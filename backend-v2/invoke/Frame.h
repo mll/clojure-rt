f893dab4211e24caf8ed2294ba053f670e285df6
Marek Lipert <marek@spinney.io>
2025-12-29 22:36:29 +0100

    Add frame definition.

---
+++ backend-v2/invoke/Frame.h
@@ -0,0 +1,26 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+extern "C" {
+#include "../runtime/ObjectProto.h"
+#include "../runtime/Object.h"
+#include "../runtime/RTValue.h"
+#include "../runtime/Function.h"
+}
+
+namespace rt {
+  struct Frame {
+    // Frame linking might not be needed? - we keep this comment as a possible concept
+    // Frame* callerFrame;    
+    
+    FunctionMethod *method;
+    RTValue variadicSeq;         
+    
+    int bailoutEntryIndex;     
+    int localsCount;           
+    RTValue locals[];           
+  };
+
+} // namespace rt
+
+#endif

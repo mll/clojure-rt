57078195dfb920b3e426e28b19ab2068710ccf66
Marek Lipert <marek@spinney.io>
2026-01-27 21:07:36 +0100

    Analyzer that re-uses internal state.

--- frontend/src/clojure/rt/core.clj
+++ frontend/src/clojure/rt/core.clj
@@ -110,44 +110,58 @@
    :validate/wrong-tag-handler      (fn [_ _] nil)
    :validate/unresolvable-symbol-handler (fn [_ _ _] nil)})
 
-(defn analyze [s filename]
-  (with-bindings {#'a/run-passes run-passes}
-    (with-redefs [ana/parse-quote quote/parse-quote
-                  a/parse-deftype* deftype/parse-deftype*
-                  annotate-host-info deftype/annotate-host-info
-                  validate-call (fn [ast] ast) #_deftype/validate-call
-                  validate-interfaces (fn [_])]
-      (let [reader (t/source-logging-push-back-reader s 1 filename)]
-        (loop [form (r/read {:eof :eof} reader) ret-val []]
-          (if (= :eof form) (do
-                              ;; Uncomment to see a trivial tree:
-;;                               (print-readable-tree ret-val 1)  
-                              ;; Uncomment to see a simple tree:
-                              ;; (clojure.pprint/pprint (identity #_passes/clean-tree ret-val)) 
-                              ret-val)
-              (do
-                (eval form)
-                (recur (r/read {:eof :eof} reader)
-                     (->> 
-                      (a/analyze form (a/empty-env) {:passes-opts passes-opts})      
-                      (conj ret-val))))))))))
+(defn analyze
+  ([s filename] (analyze s filename false false))
+  ([s filename trivial-tree? simple-tree?]
+   (with-bindings {#'a/run-passes run-passes}
+     (with-redefs [ana/parse-quote quote/parse-quote
+                   a/parse-deftype* deftype/parse-deftype*
+                   annotate-host-info deftype/annotate-host-info
+                   validate-call (fn [ast] ast)
+                   validate-interfaces (fn [_])]
+       (let [reader (t/source-logging-push-back-reader s 1 filename)]
+         (loop [form (r/read {:eof :eof} reader) 
+                current-env (a/empty-env) 
+                ret-val []]
+           (if (= :eof form)
+             (do
+               (when trivial-tree? (print-readable-tree ret-val 1))
+               (when simple-tree? (clojure.pprint/pprint ret-val))
+               ret-val)
+             (let [;; 1. Analyze the form with the accumulated environment
+                   ast (a/analyze form current-env {:passes-opts passes-opts})]
+               
+               ;; 2. Selective Eval for Macros
+               ;; If the AST represents a macro definition, we MUST eval it.
+               ;; Without this, the next form cannot be macro-expanded.
+               (when (or (and (= (:op ast) :def) (:macro (:var ast)))
+                         (and (= (:op ast) :do) ; Handle (do (defmacro ...))
+                              (some #(and (= (:op %) :def) (:macro (:var %))) (:statements ast))))
+                 (eval form))
 
+               (recur (r/read {:eof :eof} reader)
+                      ;; 3. Thread the updated environment to the next iteration
+                      (:env ast) 
+                      (conj ret-val ast))))))))))
 
 (defn generate-protobuf-defs [] (sch/generate-protobuf-defs "bytecode.proto"))
 
 ;; (compile "(+ 3 2)" "bytecode.clb")
-(defn compile [form outfile infile-name]
-  (-> form
-      (analyze infile-name)
-      (enc/encode-ast)
-      (protojure/->pb)
-      (clojure.java.io/copy (java.io.File. outfile))))
+(defn compile
+  ([form outfile infile-name] (compile form outfile infile-name false false))
+  ([form outfile infile-name trivial-tree? simple-tree?]
+   (-> form
+       (analyze infile-name trivial-tree? simple-tree?)
+       (enc/encode-ast)
+       (protojure/->pb)
+       (clojure.java.io/copy (java.io.File. outfile)))))
 
 (defn -main
   ([infile] (let [parts (split infile #"\.")]
               (compile (slurp "resources/rt-protocols.edn") "../backend-v2/rt-protocols.cljb" infile)
               (compile (slurp "resources/rt-classes.edn") "../backend-v2/rt-classes.cljb" infile)
-              (compile (slurp infile) (str (join "." (butlast parts)) ".cljb") infile)
+              (compile (slurp infile) (str (join "." (butlast parts)) ".cljb") infile
+                       false true)
               ))
   ([] (println "Generating protobuf definitions into bytecode.proto file. To compile use file name as parameter")
       (generate-protobuf-defs)))
